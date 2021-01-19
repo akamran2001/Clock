08555770a480dbd7dc67f0cb7be1fa98253648ba
Ahmed Muyen Kamran <akamran@colgate.edu>
2021-01-19 01:28:25 -0500

    background imaged to App.js and div to 100vh

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
 import Clock from "./app/Clock";
 function App() {
-  return <Clock />;
+  return (
+    <div
+      style={{
+        backgroundImage: `url(https://source.unsplash.com/${window.innerWidth}x${window.innerHeight}/daily?scenic)`,
+        height: "100vh",
+      }}
+    >
+      <Clock />
+    </div>
+  );
 }
 
 export default App;
