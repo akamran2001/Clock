5de296cf13c0840e57854b56b4e28c8352819e22
Ahmed Muyen Kamran <akamran@colgate.edu>
2021-01-20 01:18:55 -0500

    added keys to child elements in display h1 elems

--- src/app/Display.js
+++ src/app/Display.js
@@ -2,11 +2,12 @@ import React from "react";
 import "../index.css";
 import "../App.css";
 function Display(props) {
-  const getDigit = (digit) => {
+  const getDigit = (digit, index) => {
     //Display Clock digit or letter
     if (digit === ":") {
       return (
         <h1
+          key={index}
           id="digits"
           style={props.sec % 2 === 0 ? { color: "white" } : { color: "red" }}
         >
@@ -16,6 +17,7 @@ function Display(props) {
     } else if (digit === "A") {
       return (
         <h1
+          key={index}
           id="period"
           style={props.sec % 2 === 0 ? { color: "red" } : { color: "white" }}
         >
@@ -25,6 +27,7 @@ function Display(props) {
     } else if (digit === "P") {
       return (
         <h1
+          key={index}
           id="period"
           style={props.sec % 2 === 0 ? { color: "red" } : { color: "white" }}
         >
@@ -32,19 +35,27 @@ function Display(props) {
         </h1>
       );
     } else if (digit === " ") {
-      return <h1 id="period"> </h1>;
+      return (
+        <h1 key={index} id="period">
+          {" "}
+        </h1>
+      );
     } else if (digit === "M") {
       return null;
     } else {
-      return <h1 id="digits">{digit}</h1>;
+      return (
+        <h1 key={index} id="digits">
+          {digit}
+        </h1>
+      );
     }
   };
   //Render JSX
   return (
     <div className="row">
       <div className="d-flex justify-content-center clock-text col" id="time">
-        {props.time.split("").map((value) => {
-          return getDigit(value);
+        {props.time.split("").map((value, index) => {
+          return getDigit(value, index);
         })}
       </div>
     </div>
